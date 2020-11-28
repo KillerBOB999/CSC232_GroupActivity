eb4d7daedf2803530c3c4890d0d618650f7262ad
sahej2501 <bhatia.sahejbir.01@gmail.comgit config --global user.name Sahej>
2020-11-27 19:40:32 -0500

    Added Sahej

--- Greeting.h
+++ Greeting.h
@@ -22,6 +22,7 @@ struct Greeting
 	names.push_back("Ethan Tanner");
 	names.push_back("Aaron Gerbrandt");
         names.push_back("Zijing Zhao");
+        names.push_back("Sahej Bhatia");
 
     };
 
